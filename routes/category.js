d0cb94aa50c094cb702e82a3cda9d9cb270d1db7
Kqyang29 <yangkq0417@qq.com>
2023-01-28 23:25:51 -0800

    blog_backend

--- routes/category.js
+++ routes/category.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const Category = require("../models/category");
+const Category = require("../models/Category");
 
 // Create Category
 router.post("/", async (req, res) => {
